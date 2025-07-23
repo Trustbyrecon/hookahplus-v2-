2367f84fd442f7b68a1f5b59710bb4764b90f637
reconai-dev <clark.dwayne@gmail.com>
2025-07-23 18:19:38 -0400

    Update page.js

--- src/app/flavor-mix/page.js
+++ src/app/flavor-mix/page.js
@@ -1,1 +1,10 @@
-"use client"
+"use client";
+
+import { useEffect } from 'react';
+
+export default function FlavorMix() {
+  useEffect(() => {
+    console.log("runs on client");
+  }, []);
+  return <div>Flavor Mix Page</div>;
+}
