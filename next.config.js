8e1e166c5774da0eb4fedfd846c9498b44cd16ea
reconai-dev <clark.dwayne@gmail.com>
2025-07-24 08:29:36 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,3 +5,11 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+};
+
+module.exports = nextConfig;
